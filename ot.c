f1db6fa7877bdad530d49156d34113f4b617a61b
Greg Curtis <greg.r.curtis@gmail.com>
2013-08-25 14:10:12 -0700

    Import sources from old project

---
+++ ot.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <string.h>
+#include "ot.h"
+
+static void ot_ensure_size(ot_op* op) {
+	if (op->comp_count == 0) {
+		op->comp_cap = 1;
+		op->comps = malloc(sizeof(ot_comp));
+	} else if (op->comp_count == op->comp_cap) {
+		op->comp_cap = op->comp_cap * 2;
+		op->comps = realloc(op->comps, sizeof(ot_comp) * op->comp_cap);
+	}
+}
+
+static ot_comp* ot_append_new_comp(ot_op* op) {
+	ot_ensure_size(op);
+	ot_comp* new = &op->comps[op->comp_count];
+	op->comp_count++;
+	return new;
+}
+
+static ot_comp* ot_append_comp(ot_op* op, ot_comp* comp) {
+	ot_ensure_size(op);
+	op->comps[op->comp_count] = *comp;
+	ot_comp* appended = &op->comps[op->comp_count];
+	op->comp_count++;
+	return appended;
+}
+
+ot_op* ot_new_op(int64_t client_id, int64_t* parent) {
+	ot_op* op = (ot_op*) malloc(sizeof(ot_op));
+	op->client_id = client_id;
+	op->comp_count = 0;
+	op->comp_cap = 0;
+	op->comps = NULL;
+
+	memcpy(op->parent, parent, sizeof(int64_t) * 8);
+	return op;
+}
+
+void ot_insert(ot_op* op, uint8_t* text) {
+	ot_comp* comp = ot_append_new_comp(op);
+	comp->type = INSERT;
+	comp->value.insert.text = rope_new_with_utf8(text);
+}
+
+uint8_t* ot_snapshot(ot_op* op) {
+	uint8_t* out = malloc(sizeof(uint8_t) * 100);
+
+	int pos = 0;
+	for (int i = 0; i < op->comp_count; ++i)
+	{
+		if (op->comps[i].type == INSERT) {
+			pos += rope_write_cstr(op->comps[i].value.insert.text, &out[pos]) - 1;
+		}
+	}
+
+	return out;
+}
---
+++ ot.h
@@ -0,0 +1,97 @@
+#ifndef OT_H
+#define OT_H
+
+#include <stdint.h>
+#include "librope/rope.h"
+
+/*
+
+<op> ::= <int> <parent> <comp> <op>
+<comp> ::= <num> | <type>
+<num> ::= <int> <type>
+<type> ::= <ins> | "d" | "r"
+<ins> ::= "i" <str>
+
+int 0
+char[64] 0xa840c0d1e
+int64 length
+char i
+length Hello, world!
+
+*/
+
+typedef struct ot_fmt {
+	uint8_t* name;
+	uint8_t* value;
+} ot_fmt;
+
+typedef enum {
+	SKIP = 0,
+	INSERT = 1,
+	DELETE = 2,
+	OPEN_ELEMENT = 3,
+	CLOSE_ELEMENT = 4,
+	FORMATTING_BOUNDARY = 5
+} ot_comp_type;
+
+typedef struct ot_comp_skip {
+	int64_t count;
+} ot_comp_skip;
+
+typedef struct ot_comp_insert
+{
+	rope* text;
+} ot_comp_insert;
+
+typedef struct ot_comp_delete
+{
+	int64_t count;
+} ot_comp_delete;
+
+typedef struct ot_comp_open_element
+{
+	uint8_t* elem;
+} ot_comp_open_element;
+
+typedef struct ot_comp_fmtbound
+{
+	ot_fmt* start;
+	ot_fmt* end;
+} ot_comp_fmtbound;
+
+typedef struct ot_comp {
+	ot_comp_type type;
+	union {
+		ot_comp_skip skip;
+		ot_comp_insert insert;
+		ot_comp_delete delete;
+		ot_comp_open_element open_element;
+		ot_comp_fmtbound fmtbound;
+	} value;
+} ot_comp;
+
+typedef struct ot_op {
+	int64_t client_id;
+	int64_t parent[8];
+	int64_t comp_count;
+	int64_t comp_cap;
+	ot_comp* comps;	
+} ot_op;
+
+ot_op* ot_new_op(int64_t client_id, int64_t* parent);
+ot_op* ot_free_op(ot_op* op);
+void ot_skip(ot_op* op, int64_t count);
+void ot_insert(ot_op* op, uint8_t* text);
+void ot_delete(ot_op* op, int64_t count);
+void ot_open_element(ot_op* op, uint8_t* elem);
+void ot_close_element(ot_op* op);
+void ot_fmtbound(ot_op* op, ot_comp_fmtbound fmtbound);
+uint8_t* snapshot(ot_op op);
+
+ot_comp_fmtbound* ot_new_fmtbound();
+void ot_start_fmt(ot_comp_fmtbound* fmtbound, uint8_t* name, uint8_t* value);
+void ot_end_fmt(ot_comp_fmtbound* fmtbound, uint8_t* name, uint8_t* value);
+
+uint8_t* ot_snapshot(ot_op* op);
+
+#endif
