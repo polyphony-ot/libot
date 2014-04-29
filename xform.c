71ea103db1aaaacfb8c07503c4fbf34e09710f89
Greg Curtis <greg.r.curtis@gmail.com>
2014-04-28 18:11:51 -0700

    Add xform source and header

---
+++ xform.c
@@ -0,0 +1,5 @@
+#include "xform.h"
+
+ot_op* ot_xform(ot_op* op1, ot_op* op2) {
+    return NULL;
+}
---
+++ xform.h
@@ -0,0 +1,9 @@
+#ifndef libot_xform_h
+#define libot_xform_h
+
+#include <stdlib.h>
+#include "ot.h"
+
+ot_op* ot_xform(ot_op* op1, ot_op* op2);
+
+#endif
