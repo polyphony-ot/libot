cce235047d5c94d35d4de9c211a340ced460123e
Greg Curtis <greg.r.curtis@gmail.com>
2014-06-15 16:39:19 -0700

    Remove colors from scenario tests
    This was nice to have, but it makes output messy on anything that isn't
    a terminal.

--- test/scenarios/test.c
+++ test/scenarios/test.c
@@ -1,24 +1,20 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_GREEN "\x1b[32m"
-#define ANSI_COLOR_RESET "\x1b[0m"
-
-#define RUN_SCENARIO(f)                                    \
-    bool f(char**);                                        \
-    ++total;                                               \
-    msg = NULL;                                            \
-    printf("Running %s... ", #f);                          \
-    if (f(&msg)) {                                         \
-        puts(ANSI_COLOR_GREEN "passed." ANSI_COLOR_RESET); \
-        ++passed;                                          \
-    } else {                                               \
-        puts(ANSI_COLOR_RED "failed." ANSI_COLOR_RESET);   \
-        ++failed;                                          \
-    }                                                      \
-    if (msg) {                                             \
-        printf("\t%s\n", msg);                             \
+#define RUN_SCENARIO(f)           \
+    bool f(char**);               \
+    ++total;                      \
+    msg = NULL;                   \
+    printf("Running %s... ", #f); \
+    if (f(&msg)) {                \
+        puts("passed.");          \
+        ++passed;                 \
+    } else {                      \
+        puts("failed.");          \
+        ++failed;                 \
+    }                             \
+    if (msg) {                    \
+        printf("\t%s\n", msg);    \
     }
 
 char* msg = NULL;
@@ -29,18 +25,10 @@ int total = 0;
 int main() {
     RUN_SCENARIO(scenario1);
 
-    char* pass_color = "";
-    char* fail_color = "";
-    if (failed == 0) {
-        pass_color = ANSI_COLOR_GREEN;
-    } else {
-        fail_color = ANSI_COLOR_RED;
-    }
-
-    printf("\n%s%d tests passed." ANSI_COLOR_RESET "\n"
-           "%s%d tests failed." ANSI_COLOR_RESET "\n"
+    printf("\n%d tests passed.\n"
+           "%d tests failed.\n"
            "%d tests total.\n",
-           pass_color, passed, fail_color, failed, total);
+           passed, failed, total);
 
     if (failed > 0) {
         return 1;
